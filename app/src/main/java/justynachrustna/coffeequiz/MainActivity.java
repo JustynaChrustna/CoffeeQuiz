c35d52d57909f6576a4f7014d71d67aa78ef507c
Justyna Chrustna <justyna.chrustna@gmail.com>
2017-11-13 17:12:57 +0100

    Initial commit

---
+++ app/src/main/java/justynachrustna/coffeequiz/MainActivity.java
@@ -0,0 +1,16 @@
+package justynachrustna.coffeequiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+    int score=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
